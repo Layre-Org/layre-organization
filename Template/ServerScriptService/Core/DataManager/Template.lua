38705826e06d2bac34422c24efa70af615a24ef1
Karell Lutkmeier <kareudeveloper@gmail.com>
2025-10-09 17:37:33 -0300

    refactor(data): melhorias no Template
    Template de data agora possui uma categorização básica e funcional

--- Template/ServerScriptService/Core/DataManager/Template.lua
+++ Template/ServerScriptService/Core/DataManager/Template.lua
@@ -1,3 +1,22 @@
 return {
-	Coins = 0
+	['Stats'] = {
+		Coins = 0
+	},
+	
+	['Inventory'] = {
+		Infos = {
+			TotalItems = 1,
+			MaxItems = 25,
+			MaxEquippedItems = 1,
+			CurrentlyEquippedAmount = 0,
+			CurrentlyEquippedIDs = {}
+		},
+		
+		Items = {
+			--Example = {
+			--	Amount = 1,
+			--	Rarity = 'Common'
+			--}
+		}
+	},
 }
